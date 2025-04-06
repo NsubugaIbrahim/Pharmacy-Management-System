3f1d43749f4c7cdcfc62720fd237a3c0c134c0e3
kayiwarahim <kayiwarahim@gmail.com>
2025-04-06 20:22:37 +0300

    updated suppliers controller

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Supplier;
+
+class SupplierController extends Controller
+{
+    public function index()
+    {
+        $suppliers = Supplier::all();
+        return view('suppliers.index', compact('suppliers'));
+    }
+    public function create()
+    {
+        return view('suppliers.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        Supplier::create($request->all());
+
+        return redirect()->route('suppliers.index')->with('success', 'Supplier created successfully.');
+    }
+    public function edit($id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        return view('suppliers.edit', compact('supplier'));
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $supplier = Supplier::findOrFail($id);
+        $supplier->update($request->all());
+
+        return redirect()->route('suppliers.index')->with('success', 'Supplier updated successfully.');
+    }
+    public function destroy($id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        $supplier->delete();
+
+        return redirect()->route('suppliers.index')->with('success', 'Supplier deleted successfully.');
+    }
+}
