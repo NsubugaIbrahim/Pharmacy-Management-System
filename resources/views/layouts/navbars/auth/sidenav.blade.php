028a7b69be85539eee50fb2476ca5fc4551ab840
NsubugaIbrahim <ibrahimnazel942@gmail.com>
2025-04-06 21:44:32 +0300

    Life logo sidebar

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -31,7 +31,7 @@
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="{{ route('home') }}"
             target="_blank">
-            <img src="./img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="/img/life.png" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">Life Pharmacy Management</span>
         </a>
     </div>
