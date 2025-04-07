15b12affea7553f386488e0bfc802a831d9a6d44
NsubugaIbrahim <ibrahimnazel942@gmail.com>
2025-04-07 17:26:16 +0300

    User table - image

--- database/migrations/2025_04_06_000000_create_users_table.php
+++ database/migrations/2025_04_06_000000_create_users_table.php
@@ -30,7 +30,6 @@ return new class extends Migration
             $table->unsignedBigInteger('role_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
-
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
