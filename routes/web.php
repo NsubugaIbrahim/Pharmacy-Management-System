2b5e9f068e0efb0155582b878cafce9be3ff3820
kayiwarahim <kayiwarahim@gmail.com>
2025-04-07 17:00:29 +0300

    merge routes from rahim

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,35 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+	use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\UserProfileController;
-use App\Http\Controllers\ResetPassword;
-use App\Http\Controllers\ChangePassword;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\PharmacistController;
-use App\Http\Controllers\MedicalAssistantController;
-use App\Http\Controllers\CashierController;
-use App\Http\Controllers\AccountantController;           
-   Route::get('/', function () {
-    return view('welcome');
-    });         
+	/*
+	|--------------------------------------------------------------------------
+	| Web Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider and all of them will
+	| be assigned to the "web" middleware group. Make something great!
+	|
+	*/
+	
+	use App\Http\Controllers\HomeController;
+	use App\Http\Controllers\PageController;
+	use App\Http\Controllers\RegisterController;
+	use App\Http\Controllers\LoginController;
+	use App\Http\Controllers\UserProfileController;
+	use App\Http\Controllers\ResetPassword;
+	use App\Http\Controllers\ChangePassword;
+	use App\Http\Controllers\AdminController;
+	use App\Http\Controllers\PharmacistController;
+	use App\Http\Controllers\MedicalAssistantController;
+	use App\Http\Controllers\CashierController;
+	use App\Http\Controllers\AccountantController;
+	use App\Http\Controllers\DrugController;
+	use App\Http\Controllers\SupplierController;
+	use App\Http\Controllers\StockController;
+
+	Route::get('/', function () {return view('auth.login');});
 	Route::get('/register', [RegisterController::class, 'create'])->middleware('guest')->name('register');
 	Route::post('/register', [RegisterController::class, 'store'])->middleware('guest')->name('register.perform');
 	Route::get('/login', [LoginController::class, 'show'])->middleware('guest')->name('login');
@@ -45,9 +48,7 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/profile-static', [PageController::class, 'profile'])->name('profile-static'); 
 	Route::get('/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
 	Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static'); 
-	Route::get('/{page}', [PageController::class, 'index'])->name('page');
 	Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-
 
 	Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 	Route::get('/pharmacist/dashboard', [PharmacistController::class, 'index'])->name('pharmacist.dashboard');
@@ -55,6 +56,37 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/cashier/dashboard', [CashierController::class, 'index'])->name('cashier.dashboard');
 	Route::get('/accountant/dashboard', [AccountantController::class, 'index'])->name('accountant.dashboard');
 
+	Route::get('/drugs', [DrugController::class, 'index'])->name('drugs.index');
+    Route::get('/drugs/add', [DrugController::class, 'addDrug'])->name('drugs.add');
+    Route::post('/drugs/add', [DrugController::class, 'storeDrug'])->name('drugs.store');
+    Route::get('/drugs/set-price', [DrugController::class, 'setSellingPriceForm'])->name('drugs.set_price');
+    Route::post('/drugs/{drug}/set-price', [DrugController::class, 'updateSellingPrice'])->name('drugs.setprice.update');
+	// In routes/web.php
+	Route::get('drugs/{drug}/edit', [DrugController::class, 'edit'])->name('drugs.edit');// In routes/web.php
+	Route::get('drugs/{drug}/sell', [DrugController::class, 'sell'])->name('drugs.sell');
+	Route::put('drugs/{drug}', [DrugController::class, 'update'])->name('drugs.update');
+	Route::delete('drugs/{drug}/delete', [DrugController::class, 'destroy'])->name('drugs.destroy');
+
+	Route::get('/suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
+	Route::get('/suppliers/create', [SupplierController::class, 'create'])->name('suppliers.create');
+	Route::post('/suppliers', [SupplierController::class, 'store'])->name('suppliers.store');
+	Route::get('/suppliers/{supplier}/edit', [SupplierController::class, 'edit'])->name('suppliers.edit');
+	Route::put('/suppliers/{supplier}', [SupplierController::class, 'update'])->name('suppliers.update');
+	Route::delete('/suppliers/{supplier}', [SupplierController::class, 'destroy'])->name('suppliers.destroy');
+
+	Route::get('/stock', [StockController::class, 'index'])->name('stock.index');
+	Route::get('/stock/create', [StockController::class, 'create'])->name('stock.create');
+	Route::post('/stock', [StockController::class, 'store'])->name('stock.store');
+	Route::get('/stock/{stockEntry}/edit', [StockController::class, 'edit'])->name('stock.edit');
+	Route::put('/stock/{stockEntry}', [StockController::class, 'update'])->name('stock.update');
+	Route::delete('/stock/{stockEntry}', [StockController::class, 'destroy'])->name('stock.destroy');
+
+
+
+
+	
+	
+	Route::get('/{page}', [PageController::class, 'index'])->name('page');
 });
 
 //create auth group for all routes that require authentication
