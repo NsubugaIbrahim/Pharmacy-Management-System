d15e01224d42493ef962fe3260e72612d9af28c1
kayiwarahim <kayiwarahim@gmail.com>
2025-04-07 18:13:21 +0300

    added user views

---
+++ resources/views/roles/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Roles'])
+
+    @include('layouts.footers.auth.footer')
+@endsection
