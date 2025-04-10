30a39c2714be50c423c08b157ce51542f87303cd
kayiwarahim <kayiwarahim@gmail.com>
2025-04-10 17:16:34 +0300

    modified views

---
+++ resources/views/pharmacist/dashboard.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.sidenav-pharmacist', ['class' => 'g-sidenav-show bg-gray-100'])
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Pharmacist'])
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="card">
+                    <div class="card-header pb-0">
+                        <h6>Welcome to the Pharmacist Dashboard</h6>
+                    </div>
+                    <div class="card-body">
+                        <p class="text-sm">Here you can manage prescriptions, oversee medication inventory, and access pharmaceutical resources.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
